01a24fda3a92e75c78fba388c2eee337713ba718
fardian0142 <156123630+fardian0142@users.noreply.github.com>
2026-09-11 16:37:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ if not BOT_TOKEN:
 
 CHANNELS = list(dict.fromkeys([
     "https://t.me/s/Config_HATunnel",
+    "https://t.me/s/ShadowProxy66",
     "https://t.me/s/xixv2ray",
     "https://t.me/s/hddify",
     "https://t.me/s/khabari_18",
